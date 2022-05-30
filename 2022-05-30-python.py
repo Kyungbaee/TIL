512fb56d6dbb415125d337759c94bb59219c62cf
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2022-05-30 17:01:12 +0900

    Create 2022-05-30-python.py

---
+++ 2022-05-30-python.py
@@ -0,0 +1,13 @@
+- glob
+- 필요없는 데이터 및 컬럼 삭제로 메모리 줄이기
+- melt
+- pd.to_numeric
+        errors
+                1) coerce = 숫자로 바꿀 수 없는 것은 Null값으로 변환
+                2) ignore = 숫자로 바꿀 수 없는 것은 무시하고 그냥 둠
+- pivot_table
+- apply
+- sns.barplot, sns.pointplot, sns.boxplot, sns.swarmplot, sns.violinplot
+- lineplot(수치형)과 pointplot(범주형)의 차이
+- Kosis 데이터
+- columns rename
