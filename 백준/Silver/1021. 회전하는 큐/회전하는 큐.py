0c00bfa8d4f1811fc7a491dad3dbb0b4296202af
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2023-02-14 21:40:20 +0900

    Update 회전하는 큐.py

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,22 +1,24 @@
-# a, b = map(int, input().split())
-# print(a+b)
 from sys import stdin
 
+# 링크드리스트에 사용할 원소
 class Node:
     def __init__(self,data,next=None):
         self.data = data
         self.next = next
 
+# 링크드리스트 및 기능
 class LinkedList:
     def __init__(self):
         self.node_cnt = 0
         self.head = None
 
+	## 첫 번째 원소 삽입
     def add_first(self, data):
         pointer = self.head
         self.head = Node(data, pointer)
         self.node_cnt += 1
 
+	## 이후 원소들 삽입
     def add_last(self, data):
         pointer = self.head
         while pointer.next:
@@ -25,6 +27,7 @@ class LinkedList:
         pointer.next = Node(data, None)
         self.node_cnt += 1
 
+	## 해당 값을 가지고 있는 원소 찾기
     def find(self, data):
         pointer = self.head
         cnt = 0
@@ -33,15 +36,12 @@ class LinkedList:
             cnt += 1
         return cnt
 
+	## 첫 번째, 원소 제거
     def pop(self):
         self.head = self.head.next
         self.node_cnt -= 1
 
-    # def print_node(self):
-    #     head = self.head
-    #     while head.next: 
-    #         head=head.next 
-
+	## 두 번째, 왼쪽 이동 
     def Left(self,L):
         for _ in range(L):
             node = self.head
@@ -51,6 +51,7 @@ class LinkedList:
             self.head = self.head.next
             node.next.next = None
 
+	## 세 번째, 오른쪽 이동
     def Right(self,R):
         for _ in range(R):
             node = self.head
@@ -63,17 +64,20 @@ class LinkedList:
 
 
 if __name__ == "__main__":
+	## 큐의 크기 N, 뽑으려 하는 수 M개
+    ## N <= 50, M <= N
     N, M = map(int, stdin.readline().split())
     ll = LinkedList()
 
     result = 0
-
+	
     for i in range(1,N+1):
         if i == 1:
             ll.add_first(i)
         else:
             ll.add_last(i)
 
+    ## 뽑고자 하는 수
     num = list(map(int, stdin.readline().split()))
 
     for i in num:
