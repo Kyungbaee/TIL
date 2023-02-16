6c876343c091e2a7e526730c867812d793119e65
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2023-02-16 23:08:00 +0900

    [Gold V] Title: AC, Time: 500 ms, Memory: 42656 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,6 +1,7 @@
 from sys import stdin
 from collections import deque
 
+# 최종 결과 출력
 def print_list(N_q):
     print("[", end = "")
     for i, n in enumerate(N_q):
@@ -10,11 +11,15 @@ def print_list(N_q):
     print("]")
 
 if __name__ == "__main__":
+    # 테스트 케이스의 개수 T 
     T = int(stdin.readline())
 
     for _ in range(T):
+        # 수행할 함수 funcs, 1 <= funcs의 길이 <= 100,000
         funcs = str(stdin.readline().strip()).replace("RR","")
+        # 배열에 들어있는 수의 개수 n, (0 ≤ n ≤ 100,000)
         N = int(stdin.readline())       
+        # 배열에 들어있는 정수
         data = stdin.readline().strip()[1:-1]
         if data != "":
             data = data.split(",")
@@ -22,14 +27,17 @@ if __name__ == "__main__":
 
         Reverse = 0
         error_check = 1
+
         for func in funcs:
+            # R : 뒤집기
             if func == "R":
                 if Reverse == 0:
                     Reverse = 1
                 else:
                     Reverse = 0
-
+            # D : 첫 번째 값 버리기
             elif func == "D":
+                # 에러 출력
                 if (len(N_q) == 0) or (N == 0):
                     print("error")
                     error_check = 0
@@ -39,7 +47,6 @@ if __name__ == "__main__":
                         N_q.pop()
                     else:
                         N_q.popleft()
-
 
         if error_check:
             if Reverse:
