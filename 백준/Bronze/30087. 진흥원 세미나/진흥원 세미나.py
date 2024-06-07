cc6424451774b125b2a8764c9ab9a23367a51fcb
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-06-07 16:55:30 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,11 @@
+class_dict = {'Algorithm':204,
+              'DataAnalysis':207,
+              'ArtificialIntelligence':302,
+              'CyberSecurity':'B101',
+              'Network':303,
+              'Startup':501,
+              'TestStrategy':105}
+
+N = int(input())
+for _ in range(N):
+    print(class_dict[input()])
