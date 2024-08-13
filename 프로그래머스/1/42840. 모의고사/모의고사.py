615ae6a4823208ec6463feb677636f28bdc2cef6
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-13 13:19:46 +0900

    [level 1] Title: 모의고사, Time: 8.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,14 @@
+def grading(case, answers):
+    choice = []
+    if case == 1: choice = [1,2,3,4,5]
+    if case == 2: choice = [2,1,2,3,2,4,2,5]
+    if case == 3: choice = [3,3,1,1,2,2,4,4,5,5]
+    answers = [1 if answers[idx]==choice[idx%len(choice)] else 0 for idx in range(len(answers))]
+    return sum(answers)
+
+def solution(answers):
+    case_1, case_2, case_3 = grading(1, answers), grading(2, answers), grading(3, answers)
+    max_score = max([case_1, case_2, case_3])
+    answer = list(filter(lambda x: x[1]==max_score, [(1,case_1),(2,case_2),(3,case_3)]))
+    return list(map(lambda x:x[0], answer))
+    
