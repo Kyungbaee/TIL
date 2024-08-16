2ffff3410558402e0b288e80275d5844aefc8637
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-16 19:56:22 +0900

    [level 1] Title: 최소직사각형, Time: 4.07 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,7 +1,9 @@
 def solution(sizes):
     w_max, h_max = 0,0
+    ## 세로 길이와 가로 길이 중 큰 값을 가로 길이로 설정
     sizes = [[ss[1], ss[0]] if ss[0]<ss[1] else ss for ss in sizes]
     
+    ## 가로 길이와 세로 길이의 MAX 탐색
     for ss in sizes:
         if w_max < ss[0]: w_max = ss[0]
         if h_max < ss[1]: h_max = ss[1]
