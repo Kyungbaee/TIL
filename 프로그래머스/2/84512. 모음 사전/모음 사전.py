8c7af42f60399d5290b0f967c7c736297dadbf78
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-16 20:04:28 +0900

    [level 2] Title: 모음 사전, Time: 1.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,5 +1,6 @@
 def solution(word):
     word_list = []
+    ## 사전 생성
     for one in ["A","E","I","O","U"]:
         word_list.append(one)
         for two in ["A","E","I","O","U"]:
@@ -10,4 +11,6 @@ def solution(word):
                     word_list.append(one+two+thr+four)
                     for five in ["A","E","I","O","U"]:
                         word_list.append(one+two+thr+four+five)
+    
+    ## 인덱스 탐색
     return word_list.index(word)+1
