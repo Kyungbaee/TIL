7c039b51090b2b5e22f3e678a9cdc0f96fff753c
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-16 20:02:04 +0900

    [level 2] Title: 카펫, Time: 94.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,5 +1,8 @@
 
 def solution(brown, yellow):
+    ## 노란색 카펫의 넓이을 기준으로 탐색
     for w in range(yellow,0,-1):
+        ## yellow % w == 0 → 길이 대비 가로 세로의 비율이 정수 나누어 떨어지는 경우
+        ## brown == (w*2+(yellow//w)*2+4) → 노란색 카펫의 넓이, 높이가 정해졌을 시, 갈색 카펫 길이
         if yellow % w == 0 and brown == (w*2+(yellow//w)*2+4):
             return [w+2, (yellow//w)+2]
