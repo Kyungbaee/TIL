452a1c756bf5a27935ab647783f9f260501a62b2
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-19 19:13:41 +0900

    [level 1] Title: 체육복, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 18일 20:56:50
+2024년 08월 19일 19:15:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,8 +1,14 @@
 from collections import defaultdict
 def solution(n, lost, reserve):
+    ## 체육복이 없는 학생은 수업에 들을 수 없음.
+    ## 앞 번호, 뒷 번호 학생에게만 빌려줄 수 있음.
     d = defaultdict(int)
     lost, reserve = sorted(lost), sorted(reserve)
     
+    ## 여분의 체육복이 있는 학생 : 2
+    ## 여분이 있으나 체육복을 잃어버린 학생 : 1
+    ## 본인 체육복만 있는 학생 : 1
+    ## 체육복을 잃어버린 학생 : 0
     for i in range(1,n+1):
         if (i in reserve) and (i in lost): d[i] = 1
         elif i in reserve: d[i] = 2
@@ -10,13 +16,16 @@ def solution(n, lost, reserve):
         else: d[i] = 1
 
     for i in lost:
+        ## 앞 번호 학생에게 체육복을 빌림.
         if d[i-1] > 1: 
             d[i-1] -= 1
             d[i] = 1
             continue
-            
+        
+        ## 뒷 번호 학생에게 체육복을 빌림.
         if i < n and d[i+1] > 1:
             d[i+1] -= 1
             d[i] = 1
-            
+    
+    ## 체육복이 있는 학생 수 반환
     return len(list(filter(lambda x: x[1]>0, d.items())))
