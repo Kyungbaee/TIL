c285442d81eab3d95814a2617aff614be1f24353
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-26 19:44:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 6762.79 ms, Memory: 20.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,4 +1,10 @@
 from collections import defaultdict, deque
+
+'''
+BFS를 이용하여 그래프 깊이 탐색
+* 1번 노드부터 시작
+* visited에 depth 값을 저장
+'''
 def search(node, n):
     visited = [0]*(n+1)
     deq = deque()
@@ -15,7 +21,10 @@ def search(node, n):
                 deq.append(i)
     return visited
     
-    
+'''
+* n개의 노드, 양방향 간선
+* 1번 노드와 가장 멀리 떨어진 노드의 개수
+'''
 def solution(n, edge):
     answer = 0
     node = defaultdict(list)
@@ -23,6 +32,8 @@ def solution(n, edge):
     for e in edge:
         node[e[0]].append(e[1])
         node[e[1]].append(e[0])
+    
+    ## 가장 멀리 떨어진 max 값 필터
     ans = search(node, n)
     ans = list(filter(lambda x:x==max(ans), ans))
     return len(ans)
