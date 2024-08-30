1389a7f410af7a4c9393496e6f2abb4166698742
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-30 18:17:40 +0900

    [level 2] Title: 타겟 넘버, Time: 400.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,16 @@
+'''
+재귀 함수
+'''
 def recursive(numbers, num, cnt, target):
     global answer
+    
+    # N개의 숫자 설정 및 Target 값과 비교
     if cnt==len(numbers):
         if num==target:
             answer += 1
         return 
     
+    # 더하거나 빼기
     recursive(numbers, num+numbers[cnt], cnt+1, target)
     recursive(numbers, num-numbers[cnt], cnt+1, target)
     
