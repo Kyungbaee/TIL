dddeef3c28cea49a446401920e7b2d4e2d730649
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-30 18:23:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.62 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,16 +1,20 @@
 from collections import deque
-
+'''
+BFS
+'''
 def solution(maps):
     n, m = len(maps)-1, len(maps[0])-1
-    move = [[-1,0], [0,-1], [1,0], [0,1]]
+    move = [[-1,0], [0,-1], [1,0], [0,1]] ## 상,하,좌,우 이동
     deq = deque([[0,0]])
     maps[0][0] = -1
     
     while deq:
         vn, vm = deq.popleft()
+        ## 도착한 경우
         if vn==n and vm==m:
             return maps[n][m]*(-1)
         
+        ## 상,하,좌,우로 이동
         for mm in move:
             nn, nm = vn+mm[0], vm+mm[1]
             if (0<=nn<=n) and (0<=nm<=m) and (maps[nn][nm]==1):
