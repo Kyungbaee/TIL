eb69b99cf15534c3c2ef4811ce3c594de540a8c9
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-08-30 18:25:59 +0900

    [level 3] Title: 단어 변환, Time: 0.52 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,3 +1,6 @@
+'''
+현재 단어를 기준으로 한 글자만 바꿔서 만들 수 있는 글자들 찾기
+'''
 def next_list(word, words):
     w_list = []
     for i in range(len(words)):
@@ -9,9 +12,13 @@ def next_list(word, words):
 
     return w_list
 
+'''
+DFS
+'''
 def solution(begin, target, words):
     cnt = 0
     
+    ## 반환할 수 없는 경우
     if target not in words:
         return cnt
     
@@ -21,6 +28,7 @@ def solution(begin, target, words):
     while move:
         begin = move.pop()
         
+        ## 단어가 Target과 같은 경우
         if begin==target:
             return cnt+1
         
