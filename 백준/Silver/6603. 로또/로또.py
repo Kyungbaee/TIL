8bebf951c53b3d68ea6e70e9b286099d774ca1b6
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-10-17 00:45:06 +0900

    [Silver II] Title: 로또, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,13 +1,15 @@
-from sys import stdin
-from itertools import combinations
+def solve(cnt, cur):
+    if cnt == 6:
+        print(*ans_list)
+        return
+    
+    for i in range(cur, N[0]):
+        ans_list[cnt] = n_list[i]
+        solve(cnt+1, i+1)
 
-if __name__ == "__main__":
-    while(True):
-        inputs = list(map(int,stdin.readline().split()))
-
-        if inputs[0] == 0:
-            break
-        
-        for combi in combinations(inputs[1:],6):
-            print(*combi)
-        print()
+while (True):
+    N = list(map(int, input().split()))
+    if N[0]==0: break
+    n_list, ans_list = N[1:], [0]*6
+    solve(0, 0)
+    print()
