b876708f6d9270ef9d45531fec31900272c42c8e
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-10-17 11:39:32 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,19 +1,12 @@
-def solve(cnt, cur):
-    if cnt == L:
-        spell_cnt = 0
-        for a in ans_list:
-            if a in ('a','e','i','o','u'): spell_cnt += 1
-        
-        if spell_cnt>=1 and (L-spell_cnt)>=2:
-            print(''.join(ans_list))
-        return
-
-    for i in range(cur, C):
-        ans_list[cnt] = c_list[i]
-        solve(cnt+1, i+1)
+from itertools import combinations
 
 L, C = map(int, input().split())
 c_list = sorted(input().split())
-ans_list = [0]*L
 
-solve(0,0)
+for c in combinations(c_list, L):
+    spell_cnt = 0
+    for a in c:
+        if a in ('a','e','i','o','u'): spell_cnt += 1
+        
+    if spell_cnt>=1 and (L-spell_cnt)>=2:
+        print(''.join(c))
