966ff1e9cd8844d074267197047b1ef8808f4a35
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-10-30 23:24:52 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0); // c stream, c++ stream 중 c++ stream만 사용
+    cin.tie(0); // 버퍼 비우지 않음
+    cout << "Hello World!";
+}
