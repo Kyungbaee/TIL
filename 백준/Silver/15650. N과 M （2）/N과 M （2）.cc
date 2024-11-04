c4ce01487c170421550f99a9f6958a116b147de7
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-11-04 19:47:13 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+bool visited[10];
+
+void func(int k){
+    if (k==m){
+        for(int i=0; i<m; i++) cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int start = 1;
+    if (k!=0) start = arr[k-1]+1;
+    for (int i=start; i<=n; i++){
+        if(!visited[i]){
+            arr[k] = i;
+            visited[i] = 1;
+            func(k+1);
+            visited[i] = 0;
+        }
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0); // c stream, c++ stream 중 c++ stream만 사용
+    cin.tie(0); // 버퍼 비우지 않음
+
+    cin >> n >> m;
+    func(0);
+}
