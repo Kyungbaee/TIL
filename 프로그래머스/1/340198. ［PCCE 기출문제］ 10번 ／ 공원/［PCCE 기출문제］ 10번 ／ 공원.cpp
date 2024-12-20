da8e25798c637249fa191503c616d080c45f8689
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2024-12-20 13:59:02 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 132.13 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.cpp
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.cpp
@@ -1,13 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
+int chk(vector<vector<string>> park, int h, int w, int n)
+{
+    bool chk = true;
+    for(int dx=0; dx<n; dx++){
+        for(int dy=0; dy<n; dy++)
+        {
+            if(((h+dx)>=park.size() || (w+dy)>=park[0].size()) || \
+               park[h+dx][w+dy]!="-1"){
+                return -1;
+            }
+        }
+    }
+    return n;
+}
+
 int solution(vector<int> mats, vector<vector<string>> park) {
-    int answer = -1;
     sort(mats.begin(), mats.end());
+    int answer = -1;
     
     for(int i=0; i<mats.size(); i++)
     {
@@ -18,22 +32,9 @@ int solution(vector<int> mats, vector<vector<string>> park) {
             for(int w=0; w<park[0].size(); w++)
             {
                 if(park[h][w] != "-1") continue;
-                bool chk = true;
-                for(int dx=0; dx<n; dx++){
-                    for(int dy=0; dy<n; dy++)
-                    {
-                        if(((h+dx)>=park.size() || (w+dy)>=park[0].size()) || \
-                           park[h+dx][w+dy]!="-1"){
-                            chk = false;
-                            break;
-                        }
-                    }
-                    if(!chk) break;   
-                }
-                if(chk) answer = n;
+                answer = max(answer, chk(park,h,w,n));
             }
         }
     }
-    
     return answer;
 }
