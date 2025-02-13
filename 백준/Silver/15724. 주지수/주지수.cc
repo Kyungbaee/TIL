dfd463db9cf222b90d39f95112bddcce99ce0220
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2025-02-13 09:10:48 +0900

    [Silver I] Title: 주지수, Time: 100 ms, Memory: 10228 KB -BaekjoonHub

---
+++ 백준/Silver/15724. 주지수/주지수.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int jujisu[1025][1025];
+int dp[1025][1025];
+
+int main(void){
+    ios::sync_with_stdio(0); // c stream, c++ stream 중 c++ stream만 사용
+    cin.tie(0); // 버퍼 비우지 않음
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=m; j++)
+        {
+            cin >> jujisu[i][j];
+            dp[i][j] = jujisu[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
+        }
+    }
+
+    int k;
+    cin >> k;
+
+    for(int i=0; i<k; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        cout << dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1] << "\n";
+    }
+    return 0;
+}
