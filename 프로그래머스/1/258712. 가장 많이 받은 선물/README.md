11e02a9bd67309ae7c8e7ce9510983ea82dcf48d
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2025-05-20 19:40:50 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 7.63 ms, Memory: 4.38 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=cpp) 
 
 ### 성능 요약
 
-메모리: 11.9 MB, 시간: 21.07 ms
+메모리: 4.38 MB, 시간: 7.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 02일 18:34:44
+2025년 05월 20일 19:42:15
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -0,0 +1,53 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+map<string, map<string, int>> m;
+map<string, int> jisu;
+map<string, int> present;
+
+vector<string> split(string input, string delimiter) {
+	vector<string> ret;
+	long long pos = 0;
+	string token = "";
+	while ((pos = input.find(delimiter)) != string::npos) {
+		token = input.substr(0, pos);
+		ret.push_back(token);
+		input.erase(0, pos + delimiter.length());
+	}
+	ret.push_back(input);
+	return ret;
+}
+
+int solution(vector<string> friends, vector<string> gifts) {
+    
+    for(string s : gifts){
+        vector<string> sp = split(s, " ");
+        
+        m[sp[0]][sp[1]]++;
+        m[sp[1]][sp[0]]--;
+    }
+    
+    for(string s : friends){
+        for(string ss : friends){
+            if(m[s].find(ss) != m[s].end()) jisu[s] += m[s][ss];
+        }
+    }
+
+    for(string s : friends){
+        for(string ss : friends){
+            if(m[s][ss] > 0) present[s]++;
+            else if((m[s][ss] == 0) && (jisu[s] > jisu[ss])) present[s]++;
+        }
+    }
+    
+    int answer = 0;
+    for(string s : friends){
+        if(present[s] > answer) answer = present[s];
+    }
+    
+    return answer;
+}
