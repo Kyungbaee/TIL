190cb71ce9304ec8be653bed0b6257635ec761a0
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2026-03-30 21:24:51 +0900

    [Silver V] Title: 단어 정렬, Time: 52 ms, Memory: 4800 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,40 +1,21 @@
 #include <iostream>
-#include <queue>
-#include <utility>
-#include <algorithm>
+#include <set>
+#include <string>
 
 using namespace std;
 
-int main(void){
-    ios::sync_with_stdio(0); // c stream, c++ stream 중 c++ stream만 사용
-    cin.tie(0); // 버퍼 비우지 않음
+set<pair<int, string>> s;
 
-    int n;
-    cin >> n;
+int main() {
+	int n; cin >> n;
 
-    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> q;
-    vector<string> chk;
+	while (n--) {
+		string ss; cin >> ss;
+		s.insert({ ss.length(),  ss });
+	}
 
-    for(int i=0; i<n; i++)
-    {
-        string s;
-        cin >> s;
-
-        auto it = find(chk.begin(), chk.end(), s);
-        if(it == chk.end()){
-            q.push(make_pair(s.length(), s));
-            chk.push_back(s);
-        }
-    }
-
-    for(int i=0; i<chk.size(); i++)
-    {
-        auto value = q.top();
-        int size = value.first;
-        string s = value.second;
-
-        cout << s << "\n";
-        q.pop();
-    }
-    return 0;
+	for (auto it = s.begin(); it != s.end(); it++) {
+		cout << (*it).second << "\n";
+	}
+	return 0;
 }
